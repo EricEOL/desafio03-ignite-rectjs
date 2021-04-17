2662bc02d88d4688223d8d1ed062161b2fbf9afa
EricEOL <lopes.eric051@gmail.com>
2021-04-17 19:18:36 -0300

    Estilização da página Home finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 import Prismic from '@prismicio/client';
@@ -6,6 +7,8 @@ import { RichText } from 'prismic-dom';
 
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -29,13 +32,27 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ results, next_page }: PostPagination) {
   return (
-    <>
-      <img src="/Logo.svg" alt="logo"/>
-      <main className={commonStyles.container}>
-      </main>
-    </>
+    <main className={commonStyles.container}>
+      <img src="/Logo.svg" alt="logo" className={styles.logo} />
+
+      <div className={styles.posts}>
+        {results.map(post => (
+          <Link href={`/post/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <time><FiCalendar />{post.first_publication_date}</time>
+                <span><FiUser />{post.data.author}</span>
+              </div>
+            </a>
+          </Link>
+        ))}
+        <button>Carregar mais posts</button>
+      </div>
+    </main>
   )
 }
 
@@ -49,19 +66,26 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 20,
   });
 
+  const next_page = postsResponse.next_page;
+
   const posts = postsResponse.results.map(post => {
     return {
       slug: post.uid,
-      title: post.data.title,
-      updatedAt: format(new Date(post.last_publication_date), 'dd MMM yyyy', { locale: ptBR }),
+      first_publication_date: format(new Date(post.last_publication_date), 'dd MMM yyyy', { locale: ptBR }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
     }
   })
 
-  console.log(posts);
+  console.log(postsResponse);
 
   return {
     props: {
-      postsResponse
+      results: posts,
+      next_page
     }
   }
 };
