7b18f4a51aede2c5c0e571af033dea713abf1d3b
EricEOL <lopes.eric051@gmail.com>
2021-04-17 16:25:30 -0300

    Iniciando a estilização do desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +29,39 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <img src="/Logo.svg" alt="logo"/>
+      <main className={commonStyles.container}>
+      </main>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 20,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: post.data.title,
+      updatedAt: format(new Date(post.last_publication_date), 'dd MMM yyyy', { locale: ptBR }),
+    }
+  })
+
+  console.log(posts);
+
+  return {
+    props: {
+      postsResponse
+    }
+  }
+};
